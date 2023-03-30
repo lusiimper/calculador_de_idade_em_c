ebee1737b42eef3698b3bb5006b8f120a5e8ebe2
Lucas Ubiratan <65329642+lusiimper@users.noreply.github.com>
2023-03-30 14:49:37 -0300

    Add files via upload

---
+++ calculador de idade.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int mahttps://www.onlinegdb.com/#editor_1in()
+{
+    int idade;
+    int ano;
+
+    printf("que ano voce nasceu ?");
+    scanf("%d",&ano);
+    
+    idade=2023-ano;
+    
+    printf("voce tem %d",idade);
+    
+    
+}
